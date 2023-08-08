7f0ad6aa590a1be4ae315a8ac4906f6a1d3b0077
vamshi <svamshikrish02@gmail.com>
2023-08-08 20:24:04 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+//app/page.tsx
+// import { UserButton } from "@clerk/nextjs";
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <UserButton afterSignOutUrl="/"/>
+//     </div>
+//   )
+// }
+
+export default function Home() {
+  return <h1 className="head-text text-left">Home</h1>;
+  }
---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,5 @@
+function RightSidebar() {
+    return <h1>Bhvhv</h1>
+}
+
+export default RightSidebar;
